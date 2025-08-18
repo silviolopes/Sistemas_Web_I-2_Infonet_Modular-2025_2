372e9b375dee328e407bd1a9b522e18537ee55b2
silviolopes <silvio.lopes@etec.sp.gov.br>
2025-08-18 20:43:47 -0300

    Foram criadas as classes de modelagem Curso e a conexão com o banco de dados

--- escola/src/main/java/br/com/projetoescola/escola/entity/Aluno.java
+++ escola/src/main/java/br/com/projetoescola/escola/entity/Aluno.java
@@ -5,10 +5,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Aluno{
 
+    //definição dos atributos
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer idAluno;
