21bd0ac62226f5efe02910e2d7307d3adfb0d569
silviolopes <silvio.lopes@etec.sp.gov.br>
2025-08-25 21:59:03 -0300

    Adicionamos mais páginas html e criamos a controller

---
+++ escola/src/main/java/br/com/projetoescola/escola/controller/EscolaController.java
@@ -0,0 +1,17 @@
+package br.com.projetoescola.escola.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/escola")
+public class EscolaController {
+
+    @GetMapping
+    public String index(Model model) {
+        return "index";
+
+    }
+}
